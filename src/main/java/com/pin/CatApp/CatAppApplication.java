ba6ff4666a722938b4aeab0763dff569addea4d4
Blanca Rojo <blarojo@gmail.com>
2019-04-23 22:29:19 +0100

    Basic first version

---
+++ src/main/java/com/pin/CatApp/CatAppApplication.java
@@ -0,0 +1,13 @@
+package com.pin.CatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatAppApplication.class, args);
+	}
+
+}
