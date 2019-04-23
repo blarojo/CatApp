1cada92815a227f2b929313830085531a6d79131
Blanca Rojo <blarojo@gmail.com>
2019-04-23 23:22:19 +0100

    Basic controller working with Web and SpringBoot

--- src/main/java/com/pin/CatApp/CatAppApplication.java
+++ src/main/java/com/pin/CatApp/CatAppApplication.java
@@ -1,7 +1,12 @@
 package com.pin.CatApp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CatAppApplication {
@@ -10,4 +15,16 @@ public class CatAppApplication {
 		SpringApplication.run(CatAppApplication.class, args);
 	}
 
+	/*@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext appCtx){
+		return args -> {
+			System.out.println("Inspecting system beans");
+
+			String[] beanNames = appCtx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String name: beanNames) {
+				System.out.println(name);
+			}
+		};
+	}*/
 }
---
+++ src/test/java/com/pin/CatApp/HelloControllerIT.java
@@ -0,0 +1,48 @@
+package com.pin.CatApp;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIT {
+
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/hello");
+    }
+
+    @Test
+    public void shouldReturnOkInBodyWhenCallingHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo("Hello from the Cat App");
+    }
+}
